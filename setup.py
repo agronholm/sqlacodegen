d25df36e87f1730f2e0c78993cade6b233c4002c
Alex Grönholm <alex.gronholm@nextday.fi>
2014-07-17 07:03:17 +0300

    Added Python 3.4 as a supported environment

--- setup.py
+++ setup.py
@@ -47,7 +47,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
     ],
     keywords='sqlalchemy',
     license='MIT',
