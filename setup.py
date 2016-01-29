b736cc0c26bac7712633210567b4e546d2932024
Alex Grönholm <alex.gronholm@nextday.fi>
2016-01-29 02:16:40 +0200

    Added Python 3.5 to classifiers and tox environments

--- setup.py
+++ setup.py
@@ -48,7 +48,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
     ],
     keywords='sqlalchemy',
     license='MIT',
