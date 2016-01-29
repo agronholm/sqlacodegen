6607aa5e625f46d52fd986ac16ae057d27cb2491
Alex Grönholm <alex.gronholm@nextday.fi>
2016-01-29 02:25:52 +0200

    Modernized packaging and switched to setuptools_scm for versioning

--- setup.py
+++ setup.py
@@ -1,27 +1,7 @@
-import sys
 import os.path
 
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 
-import sqlacodegen
-
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
-
-
-extra_requirements = ()
-if sys.version_info < (2, 7):
-    extra_requirements = ('argparse',)
 
 here = os.path.dirname(__file__)
 readme_path = os.path.join(here, 'README.rst')
@@ -31,10 +11,13 @@ setup(
     name='sqlacodegen',
     description='Automatic model code generator for SQLAlchemy',
     long_description=readme,
-    version=sqlacodegen.__version__,
+    use_scm_version={
+        'version_scheme': 'post-release',
+        'local_scheme': 'dirty-tag'
+    },
     author='Alex Gronholm',
     author_email='sqlacodegen@nextday.fi',
-    url='http://pypi.python.org/pypi/sqlacodegen/',
+    url='https://bitbucket.org/agronholm/sqlacodegen',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -54,16 +37,20 @@ setup(
     keywords='sqlalchemy',
     license='MIT',
     packages=find_packages(exclude=['tests']),
+    setup_requires=[
+        'setuptools_scm >= 1.7.0'
+    ],
     install_requires=(
         'SQLAlchemy >= 0.6.0',
         'inflect >= 0.2.0'
-    ) + extra_requirements,
-    tests_require=['pytest', 'pytest-pep8'],
-    cmdclass={'test': PyTest},
+    ),
+    extras_require={
+        ':python_version == "2.6"': ['argparse']
+    },
     zip_safe=False,
     entry_points={
         'console_scripts': [
-            'sqlacodegen=sqlacodegen.main:main'
+            'sqlacodegen = sqlacodegen.main:main'
         ]
     }
 )
--- sqlacodegen/__init__.py
+++
@@ -1,1 +0,0 @@
-version = __version__ = '2.0.0a1'
