045821c164d5afc01b2cd41e5aa1775d0bc37ac9
emattiza <emattiza@gmail.com>
2017-01-11 21:03:53 -0600

    refactor argparsing to factory function, pull as part of main

--- sqlacodegen/main.py
+++ sqlacodegen/main.py
@@ -1,4 +1,5 @@
-from __future__ import unicode_literals, division, print_function, absolute_import
+from __future__ import unicode_literals, division, print_function, \
+    absolute_import
 import argparse
 import codecs
 import sys
@@ -10,22 +11,44 @@ import pkg_resources
 from sqlacodegen.codegen import CodeGenerator
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Generates SQLAlchemy model code from an existing database.')
-    parser.add_argument('url', nargs='?', help='SQLAlchemy url to the database')
-    parser.add_argument('--version', action='store_true', help="print the version number and exit")
-    parser.add_argument('--schema', help='load tables from an alternate schema')
-    parser.add_argument('--tables', help='tables to process (comma-separated, default: all)')
+def parser_factory():
+    '''
+    A factory method to generate a parser with standard args
+    :return: ArgumentParser
+    '''
+    parser = argparse.ArgumentParser(
+        description='Generates SQLAlchemy model '
+                    'code from an existing database.')
+    parser.add_argument('url', nargs='?',
+                        help='SQLAlchemy url to the database')
+    parser.add_argument('--version', action='store_true',
+                        help="print the version number and exit")
+    parser.add_argument('--schema',
+                        help='load tables from an alternate schema')
+    parser.add_argument('--tables',
+                        help='tables to process '
+                             '(comma-separated, default: all)')
     parser.add_argument('--skiptables', help='tables to skip ('
                                              'comma-separated, default: none)')
     parser.add_argument('--noviews', action='store_true', help="ignore views")
-    parser.add_argument('--noindexes', action='store_true', help='ignore indexes')
-    parser.add_argument('--noconstraints', action='store_true', help='ignore constraints')
-    parser.add_argument('--nojoined', action='store_true', help="don't autodetect joined table inheritance")
-    parser.add_argument('--noinflect', action='store_true', help="don't try to convert tables names to singular form")
-    parser.add_argument('--noclasses', action='store_true', help="don't generate classes, only tables")
-    parser.add_argument('--outfile', help='file to write output to (default: stdout)')
-    args = parser.parse_args()
+    parser.add_argument('--noindexes', action='store_true',
+                        help='ignore indexes')
+    parser.add_argument('--noconstraints', action='store_true',
+                        help='ignore constraints')
+    parser.add_argument('--nojoined', action='store_true',
+                        help="don't autodetect joined table inheritance")
+    parser.add_argument('--noinflect', action='store_true',
+                        help="don't try to convert "
+                             "tables names to singular form")
+    parser.add_argument('--noclasses', action='store_true',
+                        help="don't generate classes, only tables")
+    parser.add_argument('--outfile',
+                        help='file to write output to (default: stdout)')
+    return parser
+
+
+def main():
+    args = parser_factory().parse_args()
 
     if args.version:
         version = pkg_resources.get_distribution('sqlacodegen').parsed_version
@@ -44,9 +67,12 @@ def main():
         tables = None
     metadata.reflect(engine, args.schema, not args.noviews, tables)
     if args.skiptables:
-        tables = set(metadata.tables.keys())-set(args.skiptables.split(','))
+        tables = list(set(metadata.tables.keys()) - set(args.skiptables.split(
+            ',')))
         metadata.reflect(engine, args.schema, not args.noviews, tables)
-    outfile = codecs.open(args.outfile, 'w', encoding='utf-8') if args.outfile else sys.stdout
-    generator = CodeGenerator(metadata, args.noindexes, args.noconstraints, args.nojoined, args.noinflect,
+    outfile = codecs.open(args.outfile, 'w',
+                          encoding='utf-8') if args.outfile else sys.stdout
+    generator = CodeGenerator(metadata, args.noindexes, args.noconstraints,
+                              args.nojoined, args.noinflect,
                               args.noclasses)
     generator.render(outfile)
