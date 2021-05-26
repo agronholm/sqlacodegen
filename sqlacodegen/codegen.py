484578ec1a625aa02722ff4f2c67392d28d785ff
Alex Grönholm <alex.gronholm@nextday.fi>
2021-05-26 10:08:53 +0300

    Removed redundant non-capturing groups from regexes

--- sqlacodegen/codegen.py
+++ sqlacodegen/codegen.py
@@ -36,9 +36,9 @@ try:
 except ImportError:
     pass
 
-_re_boolean_check_constraint = re.compile(r"(?:(?:.*?)\.)?(.*?) IN \(0, 1\)")
-_re_column_name = re.compile(r'(?:(["`]?)(?:.*)\1\.)?(["`]?)(.*)\2')
-_re_enum_check_constraint = re.compile(r"(?:(?:.*?)\.)?(.*?) IN \((.+)\)")
+_re_boolean_check_constraint = re.compile(r"(?:.*?\.)?(.*?) IN \(0, 1\)")
+_re_column_name = re.compile(r'(?:(["`]?).*\1\.)?(["`]?)(.*)\2')
+_re_enum_check_constraint = re.compile(r"(?:.*?\.)?(.*?) IN \((.+)\)")
 _re_enum_item = re.compile(r"'(.*?)(?<!\\)'")
 _re_invalid_identifier = re.compile(r'[^a-zA-Z0-9_]' if sys.version_info[0] < 3 else r'(?u)\W')
 
