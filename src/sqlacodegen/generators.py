7dcd620423159cd319be12b63e0a97905e8c79f7
Laurent Savaete <laurent@where.tf>
2021-12-19 09:33:39 +0000

    Sort multicolumn indexes in table args (#153)

--- src/sqlacodegen/generators.py
+++ src/sqlacodegen/generators.py
@@ -270,7 +270,7 @@ class TablesGenerator(CodeGenerator):
 
             args.append(self.render_constraint(constraint))
 
-        for index in table.indexes:
+        for index in sorted(table.indexes, key=lambda i: i.name):
             # One-column indexes should be rendered as index=True on columns
             if len(index.columns) > 1:
                 args.append(self.render_index(index))
@@ -865,7 +865,7 @@ class DeclarativeGenerator(TablesGenerator):
             args.append(self.render_constraint(constraint))
 
         # Render indexes
-        for index in table.indexes:
+        for index in sorted(table.indexes, key=lambda i: i.name):
             if len(index.columns) > 1:
                 args.append(self.render_index(index))
 
