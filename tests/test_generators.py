6ca088f602af227090c4620b7dabeeacae13cce1
Daniel Kratzert <dkratzert@gmx.de>
2023-03-22 10:47:27 +0100

    Add test for double pluralized relationship

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1882,6 +1882,59 @@ back_populates='simple_item')
             """,
         )
 
+    @pytest.mark.parametrize("generator", [["use_inflect"]], indirect=True)
+    def test_use_inflect_plural_double_pluralize(self, generator: CodeGenerator) -> None:
+        Table(
+            "users",
+            generator.metadata,
+            Column("users_id", INTEGER),
+            Column("groups_id", INTEGER),
+            ForeignKeyConstraint(['groups_id'], ['groups.groups_id'], name='fk_users_groups_id'),
+            PrimaryKeyConstraint('users_id', name='users_pkey'),
+        )
+
+        Table("groups",
+              generator.metadata,
+              Column("groups_id", INTEGER),
+              Column("group_name", Text(50), nullable=False),
+              PrimaryKeyConstraint('groups_id', name='groups_pkey'),
+              )
+
+        validate_code(
+            generator.generate(),
+            ("""\
+from sqlalchemy import Column, ForeignKeyConstraint, Integer, PrimaryKeyConstraint, Text
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class Group(Base):
+    __tablename__ = 'groups'
+    __table_args__ = (
+        PrimaryKeyConstraint('groups_id', name='groups_pkey'),
+    )
+
+    groups_id = Column(Integer)
+    group_name = Column(Text(50), nullable=False)
+
+    users = relationship('User', back_populates='group')
+
+
+class User(Base):
+    __tablename__ = 'users'
+    __table_args__ = (
+        ForeignKeyConstraint(['groups_id'], ['groups.groups_id'], name='fk_users_groups_id'),
+        PrimaryKeyConstraint('users_id', name='users_pkey')
+    )
+
+    users_id = Column(Integer)
+    groups_id = Column(Integer)
+
+    group = relationship('Group', back_populates='users')
+""")
+        )
+
     def test_table_kwargs(self, generator: CodeGenerator) -> None:
         Table(
             "simple_items",
