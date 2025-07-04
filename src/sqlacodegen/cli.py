f16b7c34a54dd335ac6c3bd9fa7ceb59e3a9d09a
Patrick Hübler <patrick@eco2grow.com>
2025-07-04 23:05:59 +0200

    schemas arg now tolerates spaces between commas

--- src/sqlacodegen/cli.py
+++ src/sqlacodegen/cli.py
@@ -118,7 +118,7 @@ def main() -> None:
     engine = create_engine(args.url, **engine_args)
     metadata = MetaData()
     tables = args.tables.split(",") if args.tables else None
-    schemas = args.schemas.split(",") if args.schemas else [None]
+    schemas = [schema.strip() for schema in args.schemas.split(",")] if args.schemas else [None]
     options = set(args.options.split(",")) if args.options else set()
 
     # Instantiate the generator
