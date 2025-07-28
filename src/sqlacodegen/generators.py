e3d60987d756cfd42e20bc2b77132c239a23e4bf
Idan Sheinberg <ishinberg0@gmail.com>
2025-07-28 11:00:33 +0300

    Update src/sqlacodegen/generators.py
    Co-authored-by: Alex Grönholm <alex.gronholm@nextday.fi>

--- src/sqlacodegen/generators.py
+++ src/sqlacodegen/generators.py
@@ -275,9 +275,7 @@ class TablesGenerator(CodeGenerator):
 
             if type_.__name__ in dialect_pkg.__all__:
                 pkgname = dialect_pkgname
-        elif type_.__name__ in dir(sqlalchemy) and type_ == getattr(
-            sqlalchemy, type_.__name__
-        ):
+        elif type_ is getattr(sqlalchemy, type_.__name__, None):
             pkgname = "sqlalchemy"
         else:
             pkgname = type_.__module__
